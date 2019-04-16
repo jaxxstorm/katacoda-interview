0135b634d6a5bb68eee7af47b1cebea157459775
Lee Briggs <lbriggs@apptio.com>
2019-04-15 21:05:13 -0700

    Add verify script

--- katacodacombriggslinterview/step1-setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "I SHOULD NOT BE HERE" >> /etc/redis/redis.conf
-systemctl stop redis-server
--- katacodacombriggslinterview/step2-verify.sh
+++ katacodacombriggslinterview/step2-verify.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-curl http://localhost
+EXITCODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:3000/info)
+
+if [ $EXITCODE == "200" ];
+then
+  echo "done"
+  exit 0
+else
+  echo "error"
+  exit 1
+fi
+
