c222c8916d320f329957a5a4ccf086c56da46c4e
Lee Briggs <lbriggs@apptio.com>
2019-04-15 17:20:16 -0700

    Install mysql & apache2

--- katacodacombriggslinterview/install.sh
+++ katacodacombriggslinterview/install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "hello" > /tmp/test
+apt-get update
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password password'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password password'
+sudo apt-get -y install mysql-server
+apt-get install -y apache2 mysql-server
+systemctl start apache2
+systemctl start mysql
