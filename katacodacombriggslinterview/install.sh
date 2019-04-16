606ff4bb8cf1107075ba40e963c2644031ae71f7
Lee Briggs <lbriggs@apptio.com>
2019-04-15 19:15:23 -0700

    Untar webapp
    Also fix redis server configuration

--- katacodacombriggslinterview/install.sh
+++ katacodacombriggslinterview/install.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 apt-get update
-apt install -y redis
+apt install -y redis-server
 echo "I SHOULD NOT BE HERE" >> /etc/redis/redis.conf
+
+echo "======"
+echo "Installing webapp"
+echo "======"
+
 curl https://s3-us-west-2.amazonaws.com/techops-interview-webapp/webapp.tar.gz -o /tmp/webapp.tar.gz
+mkdir -p /var/www/webapp
+tar zxvf /tmp/webapp.tar.gz -C /var/www/webapp
+
 echo "version=3" > /tmp/version
